24c07619fc0a4a518d6cc93a94146442b694670c
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-11 13:17:16 +0330

    Added: ListItem

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,11 +54,22 @@ const Header: React.FC<Props> = ({ children, className }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button>Sign up</Button>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log in
+              </Button>
             </div>
           </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
