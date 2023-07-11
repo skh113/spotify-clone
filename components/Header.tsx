ed7e2ed73a2e317440133969237efb06d65615bb
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-11 12:49:59 +0330

    Added: Button component.

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,10 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
-import { IconSize, buttonStyle } from "@/styles/style";
+import { IconSize, buttonStyle, mobileButtonStyle } from "@/styles/style";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface Props {
   children: React.ReactNode;
@@ -23,7 +26,7 @@ const Header: React.FC<Props> = ({ children, className }) => {
         className
       )}
     >
-      <div className="w-full mb-4 items-center justify-between">
+      <div className="w-full mb-4 items-center flex justify-between">
         <div className="hidden md:flex gap-x-2 items-center ">
           <button
             title="Back"
@@ -39,6 +42,21 @@ const Header: React.FC<Props> = ({ children, className }) => {
           >
             <RxCaretRight className="text-white" size={IconSize.Large} />
           </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button title="Home" className={mobileButtonStyle}>
+            <HiHome className="text-black" size={IconSize.Small} />
+          </button>
+          <button title="Home" className={mobileButtonStyle}>
+            <BiSearch className="text-black" size={IconSize.Small} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button>Sign up</Button>
+            </div>
+          </>
         </div>
       </div>
     </div>
--- styles/style.ts
+++ styles/style.ts
@@ -6,3 +6,6 @@ export const enum IconSize {
 
 export const buttonStyle =
   "rounded-full bg-black flex items-center hover:opacity-75 transition";
+
+export const mobileButtonStyle =
+  "rounded-full p-2 bg-white flex items-center justify-center opacity-75 transition";
